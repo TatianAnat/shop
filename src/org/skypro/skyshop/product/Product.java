540fecab861017e4976d33faa591d892695a1e6e
TatianAnat <TatianAnat@yandex.ru>
2025-06-09 21:49:00 +0400

    homework online_store

---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,14 @@
+package org.skypro.skyshop.product;
+
+public class Product {
+    private String name;
+    private int productPrice;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+}
