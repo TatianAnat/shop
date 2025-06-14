502732bd18825f8a4300762b7f0723a4d7a11bfb
TatianAnat <TatianAnat@yandex.ru>
2025-06-14 14:18:47 +0400

    homework online_store_editing

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -3,45 +3,53 @@ package org.skypro.skyshop;
 import org.skypro.skyshop.basket.ProductBasket;
 import org.skypro.skyshop.product.Product;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
         ProductBasket productBasket = new ProductBasket(5); //корзина1 на 5 продуктов
         ProductBasket productBasket2 = new ProductBasket(5); //корзина2 на 5 продуктов
-//заполняем Корзину № 1
+/**
+ * заполняем Корзину № 1
+ */
         Product apple = new Product("Яблоко", 50);
         Product bread = new Product("Хлеб", 45);
         Product milk = new Product("Молоко", 100);
-        Product egg= new Product("Яйцо", 80);
-        Product oil= new Product("Масло", 30);
-        Product pear= new Product("Груша", 25);
-//заполняем Корзину № 2
-        Product cherry = new Product("Вишня", 150);
-        Product loaf = new Product("Батон", 35);
-        Product cheese = new Product("Сыр", 220);
+        Product egg = new Product("Яйцо", 80);
+        Product oil = new Product("Масло", 30);
+        Product pear = new Product("Груша", 25);
 
-//добавляем в Корзину № 1
+        /**
+         * Добавляем в Корзину № 1
+         */
+
         productBasket.addProduct(apple);
         productBasket.addProduct(bread);
         productBasket.addProduct(milk);
         productBasket.addProduct(egg);
         productBasket.addProduct(oil);
         productBasket.addProduct(pear);
-//добавляем в Корзину № 2
-        productBasket2.addProduct(cherry);
-        productBasket2.addProduct(loaf);
-        productBasket2.addProduct(cheese);
-//печатаем содкржимое Корзины № 1 и общую сумму
+
+/**
+ * Печатаем содержимое Корзины № 1 и общую сумму
+ */
         System.out.println("Корзина № 1");
         productBasket.printProductBasket();
-//печатаем содкржимое Корзины № 2 и общую сумму
+
+/**
+ * Печатаем содержимое Корзины № 2 и общую сумму
+ */
         System.out.println();
         System.out.println("Корзина № 2");
         productBasket2.printProductBasket();
 
         System.out.println();
-        System.out.println(("Есть ли хлеб в корзине? " + productBasket.hasProduct("Хлеб")));
-        System.out.println(("Есть ли мороженое в корзине? " + productBasket.hasProduct("Мороженое")));
-//очищаем Корзину № 1
+        System.out.println(("Есть ли хлеб в корзине? " + productBasket.hasProduct("хлеб")));
+        System.out.println(("Есть ли мороженое в корзине? " + productBasket.hasProduct("мороженое")));
+
+/**
+ * очищаем Корзину № 1
+ */
         productBasket.clear();
         productBasket.printProductBasket();
 
--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,66 +1,98 @@
 package org.skypro.skyshop.basket;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.skypro.skyshop.product.Product;
 
 public class ProductBasket {
-    private Product[] products;
-    private int size; // количество добавленных продуктов
+    private final Product[] products;
+    private int size;
 
     public ProductBasket(int basket) {
         products = new Product[basket];
         size = 0;
     }
-    //Метод, который добавляет продукт в корзину void ничего не возвращает
+
+    /**
+     * Метод addProduct() добавляет продукт в корзину
+     * void ничего не возвращает
+     *
+     * @param product
+     */
+    //
     public void addProduct(Product product) {
-       if (size < products.length) {
-           products[size] = product;
-           size++;
-       } else {
-           System.out.println("Корзина заполнена, невозможно добавить продукт.");
-       }
+        if (size < products.length) {
+            products[size] = product;
+            size++;
+        } else {
+            System.out.println("Корзина заполнена, невозможно добавить продукт.");
+        }
     }
 
-    // Метод для получения общей стоимости корзины
+    /**
+     * Метод для получения общей стоимости корзины
+     * Метод getTotalPrice() ничего не принимает и возвращает целое число — итоговую стоимость корзины.
+     */
+
     public int getTotalPrice() {
         int total = 0;
         for (int i = 0; i < size; i++) {
-            total += products[i].getPrice();
+            total = total + products[i].getPrice();
         }
         return total;
-}
+    }
 
-    // Метод, который печатает содержимое корзины
+    /**
+     * Метод, который печатает содержимое корзины
+     * метод printProductBasket() проверяет, пуст ли список продуктов.Если пуст, выводит "в корзине пусто"
+     * Если есть продукты, выводит каждую строку с именем и стоимостью, а в конце — общую сумму "Итого".
+     */
+
     public void printProductBasket() {
         if (size == 0) {
             System.out.println("в корзине пусто");
             return;
         }
-        for (int i = 0; i < size; i++) {
-            System.out.println(products[i].getName() + ": " + products[i].getPrice());
+        for (Product product : products) {
+            if (product != null) {
+                System.out.println(product);
+            }
+
         }
         System.out.println("Итого: " + getTotalPrice());
     }
 
-    // Метод проверки продукта в корзине по имени
+    /**
+     * Метод проверки продукта в корзине по имени
+     *
+     * @param products - список продуктов
+     * @param name     - имя продукта
+     * @return - возвращает значение true (истины), если название проверяемого продукта совпадает, с тем, что есть в корзине, иначе false
+     * equalsIgnoreCase - применяется для игнорирования регистра
+     */
+
     public boolean hasProduct(String name) {
-        for (int i = 0; i < size; i++) {
-            if (products[i].getName().equals(name)) {
+        for (Product product : products) {
+            if (product.getName().equalsIgnoreCase(name)) {
                 return true;
             }
         }
         return false;
     }
 
-    // Метод очистки корзины (проставляет null всем элементам массива)
+    /**
+     * Метод clear() - метод очистки корзины. Данный метод только очищает массив
+     * Arrays.fill(products, null) - устанавливает значение null для всех элементов массива
+     */
+
     public void clear() {
-        for (int i = 0; i < products.length; i++) {
-            products[i] = null;
-        }
-        size = 0;
+        Arrays.fill(products, null);
     }
-
-
 }
+
+
+
 
 
 
