d61104bd333a6b11623e09a9ffe4d1ea3355f613
TatianAnat <TatianAnat@yandex.ru>
2025-06-22 21:50:11 +0400

    homework online_store_search

--- src/org/skypro/skyshop/articles/Article.java
+++ src/org/skypro/skyshop/articles/Article.java
@@ -1,11 +1,17 @@
 package org.skypro.skyshop.articles;
 
-public final class Article {
+import org.skypro.skyshop.search.Searchable;
+
+public final class Article implements Searchable {
 
     private final String title;
     private final String text;
 
     public Article(String title, String text) {
+        if (title == null || text == null) {
+            throw new IllegalArgumentException("Название и текст статьи не могут быть пустыми");
+
+        }
         this.title = title;
         this.text = text;
     }
@@ -16,6 +22,26 @@ public final class Article {
 
     public String getText(){
         return text;
+    }
+
+    @Override
+    public String getSearchTerm(){
+        return this.toString();
+    }
+
+    @Override
+    public String getContentType() {
+        return "";
+    }
+
+    @Override
+    public String getName(){
+        return title;
+    }
+
+    @Override
+    public String getStringRepresentation() {
+        return getName() + " - " + getContentType();
     }
 
     @Override
--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,6 +1,8 @@
 package org.skypro.skyshop.product;
 
-public abstract class Product {
+import org.skypro.skyshop.search.Searchable;
+
+public abstract class Product implements Searchable {
     private String name;
 
     public Product(String name) {
@@ -16,6 +18,16 @@ public abstract class Product {
 
     public boolean isSpecial() {
         return false;
+    }
+
+    @Override
+    public String getSearchTerm(){
+        return name;
+    }
+
+    @Override
+    public String getContentType(){
+        return "PRODUCT";
     }
 
 }
